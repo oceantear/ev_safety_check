b1f2238ea33c98541c1f1a3925c5b8c08e83bfc7
oceantear <oceantear@gmail.com>
2018-08-11 15:37:34 +0800

    random classified files

--- src/random_select_file.py
+++ src/random_select_file.py
@@ -1,13 +1,13 @@
 import os, random
 import shutil
 
-ImagePath = ""
-MoveImgTo = ""
+ImagePath = "/home/jimmy/ev_safety_check/image/preprossedImg/alldata/nopeople/"
+MoveImgTo = "/home/jimmy/ev_safety_check/image/preprossedImg/test/nopeople/"
 
 def moveFile(fname):
     shutil.move(ImagePath + fname ,MoveImgTo + fname)
 
-for i in range(10):
-    fname = random.choice(os.listdir("/home/advrobot/ev_safety_check/train/car"))
-    print("fname =",fname)
+for i in range(2800):
+    fname = random.choice(os.listdir(ImagePath))
+    print("i =",i,"fname =",fname)
     moveFile(fname)
